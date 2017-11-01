8e93c4658973fea7b9888522de5f953e5fc25366
olso <olsansky.martin@gmail.com>
2017-11-01 22:50:25 +0100

    Add fn

---
+++ stdlib-workspace/olso/facebook-cals-alert/functions/__main__.js
@@ -0,0 +1,34 @@
+const r2 = require('r2')
+const { ICalParser } = require('cozy-ical')
+
+async function parseIcsData(icsData) {
+  const parser = new ICalParser()
+
+  return new Promise((resolve, reject) => {
+    parser.parseString(icsData, (err, cal) => {
+      if (err) {
+        return reject(err)
+      }
+
+      resolve(cal)
+    })
+  })
+}
+
+/**
+ * @param {string} icsCalendarUrl
+ * @param {string} trigger
+ * @returns {buffer}
+ */
+module.exports = async (icsCalendarUrl = '', trigger = '-P1H') => {
+  const icsData = await r2(icsCalendarUrl).text
+  const calendar = await parseIcsData(icsData)
+
+  calendar.subComponents.forEach((event) => {
+    event.addRawField('TRIGGER', '-P1H')
+  })
+
+  return new Buffer(calendar.toString(), {
+    'Content-Type': 'text/calendar; charset=utf-8'
+  })
+};
